9c8147f82bb0703997fc27a0da4e66bb4971d5b8
curoli <oliverr@broadinstitute.org>
2025-01-14 11:57:36 -0500

    Surveying data files, part 14.

--- src/file_info.rs
+++ src/file_info.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 use std::str::FromStr;
 
 pub(crate) enum FileKind {
-    Gss, Gs, F, GscOut, GscList, Gc, Pc, Pc1
+    Gss, Gs, F, GscOut, GscList, Gc, Pc, Pc1, Pc2, Pc3
 }
 pub(crate) struct FileInfo {
     kind: FileKind,
@@ -51,6 +51,8 @@ impl FromStr for FileInfo {
             ["gc", "phewas_all_large", "out"] => Ok(FileKind::Gc),
             ["pc", "phewas_all_large", "out"] => Ok(FileKind::Pc),
             ["pc", "phewas_all_large", "1", "out"] => Ok(FileKind::Pc1),
+            ["pc", "phewas_all_large", "2", "out"] => Ok(FileKind::Pc2),
+            ["pc", "phewas_all_large", "3", "out"] => Ok(FileKind::Pc3),
             _ => Err(unrecognized_path(&string)),
         }?;
         Ok(FileInfo { kind, factors })
