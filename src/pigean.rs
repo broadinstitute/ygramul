712574c094b5009f888259eee8d39f70dcb80af9
curoli <oliverr@broadinstitute.org>
2025-05-01 13:17:49 -0400

    PIGEAN to Neo4j bulk upload, part 3.

--- src/pigean.rs
+++ src/pigean.rs
@@ -8,10 +8,10 @@ enum FileKind {
     Ggss,
     Gss,
     Gs,
-    Ge
+    Ge,
 }
 
-struct File {
+struct FileInfo {
     name: String,
     pheno: String,
     kind: FileKind,
@@ -21,26 +21,45 @@ pub(crate) fn create_bulk_files(config: &PigeanConfig) -> Result<(), Error> {
     let pheno_names = pheno_names::pheno_names(&config.pheno_names)?;
     let data_files = s3::collect(&config.data_dir)?;
     for data_file in data_files {
-        if let Some(file) = classify_file(&data_file, &config.sub_dir) {
-            println!("File: {}, pheno: {}", file.name, file.pheno);
+        match classify_file(&data_file, &config.sub_dir) {
+            Some(file_info) => match file_info.kind {
+                FileKind::Ggss => {
+                    todo!()
+                }
+                FileKind::Gss => {
+                    todo!()
+                }
+                FileKind::Gs => {
+                    todo!()
+                }
+                FileKind::Ge => {
+                    todo!()
+                }
+            },
+            None => {
+                todo!()
+            }
         }
     }
     todo!()
 }
 
-fn classify_file(file: &str, sub_dir: &str) -> Option<File> {
-    if let Some((_, pheno, sub, local)) = rsplit_thrice(file) {
+fn classify_file(file: &str, sub_dir: &str) -> Option<FileInfo> {
+    if let Some((pheno, sub, local)) = last_three_parts(file) {
         if sub == sub_dir {
             let pheno = pheno.to_string();
-            let file_kind = 
-                match local {
-                    "ggss.out"       => FileKind::Ggss,
-                    "gss.out" => FileKind::Gss,
-                    "gs.out" => FileKind::Gs,
-                    "ge.out" => FileKind::Ge,
-                    _ => return None,
-                };
-            Some(File { name: file.to_string(), pheno, kind: file_kind })
+            let file_kind = match local {
+                "ggss.out" => FileKind::Ggss,
+                "gss.out" => FileKind::Gss,
+                "gs.out" => FileKind::Gs,
+                "ge.out" => FileKind::Ge,
+                _ => return None,
+            };
+            Some(FileInfo {
+                name: file.to_string(),
+                pheno,
+                kind: file_kind,
+            })
         } else {
             None
         }
@@ -49,11 +68,11 @@ fn classify_file(file: &str, sub_dir: &str) -> Option<File> {
     }
 }
 
-fn rsplit_thrice(string: &str) -> Option<(&str, &str, &str, &str)> {
-    if let Some((prefix, fourth)) = string.rsplit_once('/') {
-        if let Some((prefix, third)) = prefix.rsplit_once('/') {
-            if let Some((first, second)) = prefix.rsplit_once('/') {
-                Some((first, second, third, fourth))
+fn last_three_parts(string: &str) -> Option<(&str, &str, &str)> {
+    if let Some((prefix, third)) = string.rsplit_once('/') {
+        if let Some((prefix, second)) = prefix.rsplit_once('/') {
+            if let Some((_, first)) = prefix.rsplit_once('/') {
+                Some((first, second, third))
             } else {
                 None
             }
@@ -64,5 +83,3 @@ fn rsplit_thrice(string: &str) -> Option<(&str, &str, &str, &str)> {
         None
     }
 }
-
-
